096342ade0af0e721055a12de4fb46799f9f638f
Garth McRae <garthmcrae@gmail.com>
2024-12-15 18:23:47 +1100

    Revert.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,7 +2,6 @@ import { CSSProperties, ReactNode } from "react";
 
 const heading: CSSProperties = {
   fontFamily: '"Instrument Serif", serif',
-  // fontFamily: "serif",
   fontSize: 32,
   fontStyle: "normal",
   fontWeight: 400,
